3fefaf2d96954410801f06980a234edbf14aee9b
keviljh3 <kevinljh3@hotmail.com>
2018-08-26 08:58:21 +0800

    Update header.php

--- web/header.php
+++ web/header.php
@@ -12,7 +12,7 @@ error_reporting( E_ALL&~E_NOTICE );
 
 <meta content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0" name="viewport" />
 <link rel="stylesheet" href="//libs.cdnjs.net/font-awesome/4.7.0/css/font-awesome.min.css" >
-<link href="https://libs.cdnjs.net/Swiper/4.0.6/css/swiper.min.css" rel="stylesheet"> 
+<link href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.0.6/css/swiper.min.css" rel="stylesheet"> 
 <script src="//libs.cdnjs.net/jquery/2.1.4/jquery.js"></script>
 <script src="//libs.cdnjs.net/jqueryui/1.11.2/jquery-ui.js"></script>
 <link href="//libs.cdnjs.net/video.js/5.20.4/alt/video-js-cdn.min.css" rel="stylesheet" />
